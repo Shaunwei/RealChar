f72d24712f9a0955a5352f69a1da3e8b6be6b963
Joey Wang <z@figma.com>
2023-07-09 15:42:30 -0700

    Hello world of iOS app

---
+++ app/ios/rac/rac/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  rac
+//
+//  Created by ZongZiWang on 7/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/ios/rac/rac/racApp.swift
@@ -0,0 +1,17 @@
+//
+//  racApp.swift
+//  rac
+//
+//  Created by ZongZiWang on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct racApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

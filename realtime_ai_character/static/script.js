d659d7abbf776ce2e1788f4884e19cf914a4b77b
shaun <shaunweix@gmail.com>
2023-07-13 22:05:10 -0700

    init auth

--- realtime_ai_character/static/script.js
+++ realtime_ai_character/static/script.js
@@ -1,3 +1,11 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+  ],
+});
 /**
  * WebSocket Connection
  * The client sends and receives messages through this WebSocket connection.

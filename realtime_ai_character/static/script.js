a4c8abf2ee58861a73fb220a8c5a33234ca3c175
shaun <shaunweix@gmail.com>
2023-07-16 02:00:19 -0700

    add bruce image

--- realtime_ai_character/static/script.js
+++ realtime_ai_character/static/script.js
@@ -463,6 +463,7 @@ function createCharacterGroups(message) {
     'Ai Character Helper': '/static/ai_helper.png',
     'Reflection Pi': '/static/pi.jpeg',
     'Elon Musk': '/static/elon.png',
+    'Bruce Wayne': '/static/bruce.png',
     'Steve Jobs': '/static/jobs.png',
   };
 

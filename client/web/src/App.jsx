eb76599610a541879e3d0fccca221adbd64ab9f1
Joey Wang <z@figma.com>
2023-07-17 14:49:11 -0700

    Fix websocket connection

--- client/web/src/App.jsx
+++ client/web/src/App.jsx
@@ -124,7 +124,23 @@ const App = () => {
     const clientId = Math.floor(Math.random() * 1010000);
     const ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
     // const ws_path = ws_scheme + '://realchar.ai:8000/ws/' + clientId;
-    const ws_path = ws_scheme + '://' + window.location.host + `/ws/${clientId}`;
+    // Get the current host value
+    var currentHost = window.location.host;
+
+    // Split the host into IP and port number
+    var parts = currentHost.split(':');
+
+    // Extract the IP address and port number
+    var ipAddress = parts[0];
+    var currentPort = parts[1];
+
+    // Define the new port number
+    var newPort = '8000';
+
+    // Generate the new host value with the same IP but different port
+    var newHost = ipAddress + ':' + newPort;
+
+    const ws_path = ws_scheme + '://' + newHost + `/ws/${clientId}`;
     console.log(`ws_path ${ws_path}`);
 
     let sk = new WebSocket(ws_path);

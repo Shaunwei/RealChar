d4a0fb4fc9fe9a0748f3051a56a232f220c9caf9
ahmed <ahmedbadawi12345678910@gmail.com>
2023-07-19 18:26:31 +0300

    ss

--- realtime_ai_character/audio/speech_to_text/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class SpeechToText(ABC):
-    @abstractmethod
-    def transcribe(self, audio_bytes, platform='web', prompt='') -> str:
-        # platform: 'web' | 'mobile' | 'terminal'
-        pass
--- realtime_ai_character/audio/text_to_speech/base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class TextToSpeech(ABC):
-    @abstractmethod
-    async def stream(self, *args, **kwargs):
-        pass

c367ea739cf7d9e2e4a3d555e038e2d738a89fba
Piaoyang Cui <bcstyle@gmail.com>
2023-07-18 22:53:54 -0700

    Add session IDs to table

---
+++ alembic/versions/3821f7adaca9_add_session_id.py
@@ -0,0 +1,24 @@
+"""Add session ID
+
+Revision ID: 3821f7adaca9
+Revises: 27fe156a6d72
+Create Date: 2023-07-18 22:44:33.107380
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3821f7adaca9'
+down_revision = '27fe156a6d72'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('interactions', sa.Column('session_id', sa.String(50), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('interactions', 'session_id')

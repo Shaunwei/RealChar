a22b7610c91564d29ae67cf4721eddc3bd404018
Piaoyang Cui <bcstyle@gmail.com>
2023-07-18 22:42:53 -0700

    Change messages to use Unicode

--- realtime_ai_character/models/interaction.py
+++ realtime_ai_character/models/interaction.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Unicode
 import datetime
 from realtime_ai_character.database.base import Base
 
@@ -10,6 +10,9 @@ class Interaction(Base):
     client_id = Column(Integer)
     client_message = Column(String)
     server_message = Column(String)
+    client_message_unicode = Column(Unicode(65535))
+    server_message_unicode = Column(Unicode(65535))
+
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
     platform = Column(String(50))
     action_type = Column(String(50))

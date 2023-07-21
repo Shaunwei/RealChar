b6fe9728d98dfc5e984f9e02f2836e6fa158b78c
Piaoyang Cui <bcstyle@gmail.com>
2023-07-21 11:37:55 -0700

    Fix frontend

--- client/web/src/components/Auth/SignIn.js
+++ client/web/src/components/Auth/SignIn.js
@@ -16,7 +16,9 @@ export const signInWithGoogle = async (isLoggedIn, setToken) => {
     .then(async (result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
+      // const token = credential.idToken;
+      const token = await auth.currentUser.getIdToken();
+
       setToken(token);
 
       // Send token to server

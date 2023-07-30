ad6a77eb2b6b02b0de165dca61a31b12a0d86592
Kai Zhou <kevinkaizhou@gmail.com>
2023-07-29 23:27:45 -0700

    Support google search for Anthropic (#229)

--- realtime_ai_character/llm/openai_llm.py
+++ realtime_ai_character/llm/openai_llm.py
@@ -1,19 +1,15 @@
 import os
 from typing import List
 
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 if os.getenv('OPENAI_API_TYPE') == 'azure':
     from langchain.chat_models import AzureChatOpenAI
 else:
     from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
 from langchain.schema import BaseMessage, HumanMessage
 
 from realtime_ai_character.database.chroma import get_chroma
-from realtime_ai_character.llm.base import AsyncCallbackAudioHandler, AsyncCallbackTextHandler, LLM
+from realtime_ai_character.llm.base import AsyncCallbackAudioHandler, AsyncCallbackTextHandler, LLM, SearchAgent
 from realtime_ai_character.logger import get_logger
 from realtime_ai_character.utils import Character
 
@@ -38,18 +34,7 @@ class OpenaiLlm(LLM):
             )
         self.db = get_chroma()
         self.search_agent = None
-        llm = OpenAI(temperature=0)
-        tools = None
-        if os.getenv('SERPER_API_KEY'):
-            tools = load_tools(["google-serper"], llm=llm)
-        elif os.getenv('SERPAPI_API_KEY'):
-            tools = load_tools(["serpapi"], llm=llm)
-        elif os.getenv('GOOGLE_API_KEY') and os.getenv('GOOGLE_CSE_ID'):
-            tools = load_tools(["google-search"], llm=llm)
-        if tools:
-            self.search_agent = initialize_agent(
-                tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION
-            )
+        self.search_agent = SearchAgent()
 
     async def achat(self,
                     history: List[BaseMessage],
@@ -63,17 +48,7 @@ class OpenaiLlm(LLM):
         context = self._generate_context(user_input, character)
         # Get search result if enabled
         if useSearch:
-            if self.search_agent is None:
-                logger.warning('Search is not enabled, please set SERPER_API_KEY to enable it.')
-            else:
-                try:
-                    search_result: str = self.search_agent.run(character.name + ' ' + user_input)
-                    search_context = 'Search input: ' + user_input + '\n' + 'Search result: ' + search_result
-                    logger.info(f'Search result: {search_context}')
-                    # Append to context
-                    context += '\n' + search_context
-                except Exception as e:
-                    logger.error(f'Error when searching: {e}')
+            context += self.search_agent.search(user_input)
 
         # 2. Add user input to history
         history.append(HumanMessage(content=user_input_template.format(

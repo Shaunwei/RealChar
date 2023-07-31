33ae4709c9f9f335e22cde6782f50e75195b3977
lynchee-owo <lynchee.owo@gmail.com>
2023-07-30 18:26:09 -0700

    added tooltip on settings

--- client/web/src/components/CommunicationMethod/index.js
+++ client/web/src/components/CommunicationMethod/index.js
@@ -7,6 +7,9 @@
 
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import CustomTooltip from '../Common/CustomTooltip';
 import './style.css';
 
 const CommunicationMethod = ({ commMethod, setCommMethod }) => {
@@ -19,8 +22,18 @@ const CommunicationMethod = ({ commMethod, setCommMethod }) => {
 
     return (
         <>
-        <label>Communication method</label>
-        <Grid container spacing={2} sx={{ marginBottom: 5}}>  
+        <div className="title" style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
+            <span>Communication method</span>
+            <CustomTooltip title="You can change the communication method during your conversation." placement="top">
+                <IconButton 
+                    aria-label="info"
+                    sx={{ color: 'white', marginLeft: '5px', padding: '5px' }}
+                >
+                    <InfoOutlinedIcon fontSize="small" />
+                </IconButton>
+            </CustomTooltip>
+        </div>
+        <Grid container spacing={2} sx={{ marginBottom: 5 }}>  
             {communication.map( method => (
                 <Grid item xs={6}>
                 <Button

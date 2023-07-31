e03a87f4a8524c66c1837d1a8d28f98b6a311e1d
lynchee-owo <lynchee.owo@gmail.com>
2023-07-30 19:00:59 -0700

    updated user signin avatar

--- client/web/src/components/Auth/SignOut.js
+++ client/web/src/components/Auth/SignOut.js
@@ -8,13 +8,18 @@
 import React from 'react';
 import auth from '../../utils/firebase';
 import { signOut } from "firebase/auth";
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
 import './styles.css';
 
 const SignOut = ({ isLoggedIn, user, handleDisconnect }) => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const signout = async (e) => {
-    e.preventDefault();
-
+  const signout = async () => {
     signOut(auth).then(() => {
       console.log("Sign-out successful.");
       isLoggedIn.current = false;
@@ -25,11 +30,47 @@ const SignOut = ({ isLoggedIn, user, handleDisconnect }) => {
     handleDisconnect();
   }
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleSignout = async () => {
+    await signout();
+    setAnchorElUser(null);
+  }
+
   return (
-    <form onSubmit={signout} className='signout-container'>
-      <p className='text-white'>Hello, <span>{user.displayName}</span></p>
-      <button type="submit" className='auth-btn'>Sign out</button>
-    </form>
+    <div>
+      <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+        <Avatar alt={user.displayName} src={user.photoURL} />
+      </IconButton>
+      <Menu
+          sx={{ mt: '45px'}}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+          classes={{paper: 'menu-paper'}} 
+        >
+          <MenuItem className='menu-text' onClick={handleSignout}>
+            <LogoutIcon />
+            <Typography textAlign="center">Log out</Typography>
+          </MenuItem>
+        </Menu>
+      </div>
   )
 }
 

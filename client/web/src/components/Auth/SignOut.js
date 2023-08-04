4e52d47e825d0a18249f858161bb4543e4e345e3
shaun <shaunweix@gmail.com>
2023-08-04 16:57:14 -0700

    clean up code

--- client/web/src/components/Auth/SignOut.js
+++ client/web/src/components/Auth/SignOut.js
@@ -1,110 +1,94 @@
 /**
  * src/components/Auth/SignOut.jsx
  * sign out
- * 
+ *
  * created by Lynchee on 7/20/23
  */
 
 import React from 'react';
 import auth from '../../utils/firebase';
-import { signOut } from "firebase/auth";
-import IconButton from '@mui/material/IconButton';
-import LogoutIcon from '@mui/icons-material/Logout';
+import { signOut } from 'firebase/auth';
 import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Typography from '@mui/material/Typography';
-import { Navbar, Dropdown } from "@nextui-org/react";
+import { Navbar, Dropdown } from '@nextui-org/react';
 import './styles.css';
 
 const SignOut = ({ isLoggedIn, user, handleDisconnect }) => {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
   const signout = async () => {
-    signOut(auth).then(() => {
-      console.log("Sign-out successful.");
-      isLoggedIn.current = false;
-    }).catch((error) => {
-      console.log(`Sign-out failed: ${error.message}`);
-    });
+    signOut(auth)
+      .then(() => {
+        console.log('Sign-out successful.');
+        isLoggedIn.current = false;
+      })
+      .catch(error => {
+        console.log(`Sign-out failed: ${error.message}`);
+      });
 
     handleDisconnect();
-  }
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   const handleSignout = async () => {
     await signout();
-    console.log('handleSignout function called');
-    setAnchorElUser(null);
-  }
+  };
 
-  const handleDropdownAction = async (actionKey) => {
-    if (actionKey === "logout") {
+  const handleDropdownAction = async actionKey => {
+    if (actionKey === 'logout') {
       // when a dropdown button is pressed using its "key" ,which we set, we can tell when its pressed , so when the key "logout" is pressed we sign the user out, in the future you can use the key "profile" to navigate the user to his dashboard for example
       await handleSignout();
-     
     }
   };
 
- 
-
   return (
-  
     <Navbar.Content
-   
-    css={{
-      "@xs": {
-        w: "12%",
-        jc: "flex-end",
-      },
-    }}
-  >
-    <Dropdown  placement="bottom-right">
-      <Navbar.Item>
-
-        {/* This is what triggers user info dropdown */}
-        <Dropdown.Trigger>
-          <Avatar
-            bordered
-            className='usericon'
-            color="warning"
-            size="md"
-            src={user.photoURL}
-            alt={user.displayName}
-          />
-        </Dropdown.Trigger>
-      </Navbar.Item>
-      <Dropdown.Menu
-        aria-label="User menu actions"
-        onAction={(actionKey) => handleDropdownAction(actionKey)}
-       
-      >{/* This ^ is probably gonna be needed for future features,actionkey tells you what dropdown the user clicked*/}
-        <Dropdown.Item className='dropdown-item' key="profile" css={{
-  height: "$18",d: "flex"
-}}>
-       
+      css={{
+        '@xs': {
+          w: '12%',
+          jc: 'flex-end',
+        },
+      }}
+    >
+      <Dropdown placement='bottom-right'>
+        <Navbar.Item>
+          {/* This is what triggers user info dropdown */}
+          <Dropdown.Trigger>
+            <Avatar
+              bordered
+              className='usericon'
+              color='warning'
+              size='md'
+              src={user.photoURL}
+              alt={user.displayName}
+            />
+          </Dropdown.Trigger>
+        </Navbar.Item>
+        <Dropdown.Menu
+          aria-label='User menu actions'
+          onAction={actionKey => handleDropdownAction(actionKey)}
+        >
+          {/* This ^ is probably gonna be needed for future features,actionkey tells you what dropdown the user clicked*/}
+          <Dropdown.Item
+            className='dropdown-item'
+            key='profile'
+            css={{
+              height: '$18',
+              d: 'flex',
+            }}
+          >
             Signed in as
-         
-       <br/>
-           {user.email}
-         
-        </Dropdown.Item>
-        <Dropdown.Item key="logout" className='dropdown-logout' withDivider color="warning">
-       Log Out
-      </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  </Navbar.Content>
- 
-  )
-}
+            <br />
+            {user.email}
+          </Dropdown.Item>
+          <Dropdown.Item
+            key='logout'
+            className='dropdown-logout'
+            withDivider
+            color='warning'
+          >
+            Log Out
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Navbar.Content>
+  );
+};
 
 export default SignOut;
-
--- client/web/src/components/Header/index.jsx
+++ client/web/src/components/Header/index.jsx
@@ -10,46 +10,46 @@ import logo from '../../assets/svgs/logo.svg';
 import './style.css';
 import SignIn from '../Auth/SignIn';
 import SignOut from '../Auth/SignOut';
-import { Navbar} from "@nextui-org/react";
+import { Navbar } from "@nextui-org/react";
 
 
 const Header = ({ user, isLoggedIn, setToken, handleDisconnect }) => (
   <Navbar id='navbar' variant="floating">
-        
-  <Navbar.Brand
- 
-    css={{
-      "@xs": {
-        w: "12%",
-      },
-    }}
-  >
-   <img src={logo} alt="Logo" />
-  
-  </Navbar.Brand>
 
-  <Navbar.Content id="navbar"
-    css={{
-      "@xs": {
-        w: "19%",
-        jc: "flex-end",
-      },
-    }}
-  > 
- 
- { user ?
-          <SignOut 
-            isLoggedIn={isLoggedIn} 
-            user={user} 
-            handleDisconnect={handleDisconnect} 
-          /> : 
-          <SignIn 
-            isLoggedIn={isLoggedIn}
-            setToken={setToken}
-          /> }
-  </Navbar.Content>
-  
-</Navbar>
+    <Navbar.Brand
+
+      css={{
+        "@xs": {
+          w: "12%",
+        },
+      }}
+    >
+      <img src={logo} alt="Logo" />
+
+    </Navbar.Brand>
+
+    <Navbar.Content id="navbar"
+      css={{
+        "@xs": {
+          w: "19%",
+          jc: "flex-end",
+        },
+      }}
+    >
+
+      {user ?
+        <SignOut
+          isLoggedIn={isLoggedIn}
+          user={user}
+          handleDisconnect={handleDisconnect}
+        /> :
+        <SignIn
+          isLoggedIn={isLoggedIn}
+          setToken={setToken}
+        />}
+    </Navbar.Content>
+
+  </Navbar>
 );
 
 export default Header;

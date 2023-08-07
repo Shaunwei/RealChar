ba4c88474faafca101afe92db3584420ba6ce8b0
shaun <shaunweix@gmail.com>
2023-08-07 15:57:15 -0700

    Update urlUtils.js

--- client/web/src/utils/urlUtils.js
+++ client/web/src/utils/urlUtils.js
@@ -4,20 +4,21 @@
  *
  * created by pycui on 08/07/23
  */
+import { isIP } from 'is-ip';
 
 export const getHostName = () => {
-    var currentHost = window.location.host;
-    var parts = currentHost.split(':');
-    var hostname = parts[0];
-    // Local deployment uses 8000 port by default.
-    var newPort = '8000';
+  var currentHost = window.location.host;
+  var parts = currentHost.split(':');
+  var hostname = parts[0];
+  // Local deployment uses 8000 port by default.
+  var newPort = '8000';
 
-    if (!(hostname === 'localhost' || isIP(hostname))) {
-      // Remove www. from hostname
-      hostname = hostname.replace('www.', '');
-      hostname = 'api.' + hostname;
-      newPort = window.location.protocol === 'https:' ? 443 : 80;
-    }
-    var newHost = hostname + ':' + newPort;
-    return newHost;
+  if (!(hostname === 'localhost' || isIP(hostname))) {
+    // Remove www. from hostname
+    hostname = hostname.replace('www.', '');
+    hostname = 'api.' + hostname;
+    newPort = window.location.protocol === 'https:' ? 443 : 80;
+  }
+  var newHost = hostname + ':' + newPort;
+  return newHost;
 };

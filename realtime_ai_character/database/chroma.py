4ca94cf4ea1862b13f8caa15a8a504d1ac408f94
Chaoyu <paranoyang@gmail.com>
2023-08-10 15:18:17 -0700

    fix: chroma embedding func expects list[float]

--- realtime_ai_character/database/chroma.py
+++ realtime_ai_character/database/chroma.py
@@ -1,3 +1,5 @@
+from typing import Callable, List
+
 import os
 from dotenv import load_dotenv
 from langchain.vectorstores import Chroma
@@ -14,11 +16,11 @@ if embedding_endpoint:
     # Use self-hosted embedding model via BentoML API endpoint
     from bentoml.client import Client
     client = Client.from_url(embedding_endpoint)
-    embedding = client.encode
+    embedding_func: Callable[[List[str]], List[float]] = lambda docs: client.encode(docs).tolist()
 else:
-    embedding = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    embedding_func = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
     if os.getenv('OPENAI_API_TYPE') == 'azure':
-        embedding = OpenAIEmbeddings(
+        embedding_func = OpenAIEmbeddings(
             openai_api_key=os.getenv("OPENAI_API_KEY"),
             deployment=os.getenv(
                 "OPENAI_API_EMBEDDING_DEPLOYMENT_NAME", "text-embedding-ada-002"
@@ -29,7 +31,7 @@ else:
 def get_chroma():
     chroma = Chroma(
         collection_name='llm',
-        embedding_function=embedding,
+        embedding_function=embedding_func,
         persist_directory='./chroma.db'
     )
     return chroma

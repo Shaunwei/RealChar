20285e73cdf98e565fd2263079f993c7c822fa00
Chaoyu <paranoyang@gmail.com>
2023-08-11 14:58:39 -0700

    Fix embeddings protocol

--- realtime_ai_character/database/chroma.py
+++ realtime_ai_character/database/chroma.py
@@ -1,23 +1,32 @@
 import os
 from dotenv import load_dotenv
+from bentoml.client import Client
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings.base import Embeddings
 from realtime_ai_character.logger import get_logger
 
 load_dotenv()
 logger = get_logger(__name__)
 
 
+class BentoEmbeddings(Embeddings):
+    def __init__(self, embedding_svc_client: Client):
+        self.client = embedding_svc_client
+
+    def embed_documents(self, texts: list[str]) -> list[list[float]]:
+        return self.client.encode(texts).tolist()
+
+    def embed_query(self, text: str) -> list[float]:
+        return self.client.encode([text]).tolist()[0]
+
+
 embedding_endpoint = os.getenv("BENTOML_EMBEDDING_ENDPOINT")
 
 if embedding_endpoint:
     # Use self-hosted embedding model via BentoML API endpoint
-    from bentoml.client import Client
     client = Client.from_url(embedding_endpoint)
-	class BentoEmbeddings:
-		def embed_documents(self, texts: list[str], chunk_size: int | None = 0) -> list[list[float]]:
-			return client.encode(docs).tolist()
-	embedding_func = BentoEmbeddings()
+    embedding_func = BentoEmbeddings(client)
 else:
     embedding_func = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
     if os.getenv('OPENAI_API_TYPE') == 'azure':

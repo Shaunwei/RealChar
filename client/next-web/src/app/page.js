a57b599e84a721f61740ebff5c5fd8527ebc3c7f
YA <yusufahmedbiz1@gmail.com>
2023-08-24 22:55:42 +0100

    removed cta box and folder/image

--- client/next-web/src/app/page.js
+++ client/next-web/src/app/page.js
@@ -1,16 +1,12 @@
 import Tabs from './_components/Tabs';
-import Ctabox from './_components/ctabox';
 
 import { getDefaultCharacters } from '../util/apiSsr';
-
-
 
 export default async function Page() {
   const characterGroup = await getDefaultCharacters();
 
   return (
       <>
-        <Ctabox/>
         <h1 className="text-center font-light text-3xl pt-10">Real-time communication with your AI character assistant</h1>
         <Tabs defaultCharacters={characterGroup} />
       </>

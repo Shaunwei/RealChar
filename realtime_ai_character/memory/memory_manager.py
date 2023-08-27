fbf5282af9fa519711f97f5071cd82644a3ff7e5
Piaoyang Cui <bcstyle@gmail.com>
2023-08-26 21:11:55 -0700

    Add a memory manager interface (#404)

---
+++ realtime_ai_character/memory/memory_manager.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+
+from realtime_ai_character.logger import get_logger
+from realtime_ai_character.utils import Singleton
+from realtime_ai_character.database.connection import get_db
+
+load_dotenv()
+logger = get_logger(__name__)
+
+
+class MemoryManager(Singleton):
+    def __init__(self):
+        super().__init__()
+        self.sql_db = next(get_db())
+
+    async def process_session(self, session_id: str):
+        # Not implemented.
+        pass
+
+
+def get_memory_manager():
+    return MemoryManager.get_instance()
+
+
+if __name__ == '__main__':
+    manager = MemoryManager.get_instance()

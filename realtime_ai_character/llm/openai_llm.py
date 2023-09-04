c5a4d8ae4eade89f3b2113179a00393d0eb9c50b
angussaw <angus.saw@temus.com>
2023-09-04 13:09:05 +0800

    Incporated converstional agent to look up meeting details and insert guest details

--- realtime_ai_character/llm/openai_llm.py
+++ realtime_ai_character/llm/openai_llm.py
@@ -8,6 +8,19 @@ else:
     from langchain.chat_models import ChatOpenAI
 from langchain.schema import BaseMessage, HumanMessage
 
+from langchain.chains import RetrievalQA
+from langchain.agents import initialize_agent, Tool, AgentExecutor, AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain import PromptTemplate
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Numeric
+import psycopg2
+
 from realtime_ai_character.database.chroma import get_chroma
 from realtime_ai_character.llm.base import AsyncCallbackAudioHandler, \
     AsyncCallbackTextHandler, LLM, QuivrAgent, SearchAgent, MultiOnAgent
@@ -43,6 +56,100 @@ class OpenaiLlm(LLM):
         self.quivr_agent = QuivrAgent()
         self.multion_agent = MultiOnAgent()
 
+        self.qa_template = """
+            Context
+            ---
+            {context}
+            ---
+            Use previous information as context to answer the following user question, Aim to keep responses super super concise and meaningful and try to express emotions.
+            ALWAYS ask clarification question, when
+            - user's question isn't clear
+            - seems unfinished
+            - seems totally irrelevant
+            Remember to prefix your reply.
+            ---
+            {query}"""
+    
+        self.qa_tool = Tool(name = "meeting_details",
+                            func = self.meeting_details,
+                            description = "Use this tool to know more about the meeting attendees and their details. The input to this tool should be a string")
+
+        self.sql_tool = Tool(name="Insert_guest_datatable",
+                        func=self.parsing_name_number,
+                        description="Use this tool when you need to insert a name and phone number into a database to take attendance of a guest.\
+                            Take attendance of the guest after he has confirmed his details.\
+                            The input to this tool should be a comma separated list of strings of length two, representing the name and the phone number you would like to insert into the database.\
+                            For example, `John,91827364` would be the input if you wanted to insert the name John and the phone number 91827364\
+                            If only `John` is provided, only insert the name John\
+                            If only `91827364` is provided, only insert the phone number 91827364")
+
+
+        self.tools = [self.sql_tool, self.qa_tool]
+
+        self.agent = initialize_agent(self.tools, 
+                                      self.chat_open_ai,
+                                      agent = AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+                                      memory =  ConversationBufferMemory(memory_key="chat_history", return_messages=True))
+        
+
+    def parsing_name_number(self, string):
+        try:
+            name, number = string.split(",")
+            return self.insert_sql_table(name = name, phone_number=int(number))
+        except:
+            return self.insert_sql_table(name=string, phone_number=None)
+
+
+    def insert_sql_table(self, name=None, phone_number=None):
+
+        # Define connection parameters
+        params = {
+            'dbname': 'temo',
+            'user': 'angus.saw',
+            'host': 'localhost',  # adjust if your server is elsewhere
+            'port': '5432'  # default port for PostgreSQL
+        }
+        table_name = "guests_test"
+
+        # check for the values we have been given
+        if name and phone_number:
+            # Establish the connection
+            try:
+                with psycopg2.connect(**params) as conn:
+                    curs_obj = conn.cursor()
+                    curs_obj.execute(f"INSERT INTO {table_name} (name, number) VALUES('{name}', {phone_number});")
+                    conn.commit()
+                    curs_obj.close()
+
+            except Exception:
+                return
+
+            return f"Inserted name: {name} number: {phone_number}"
+        
+        else:
+            return "Could not insert into database. Need name AND phone number."
+
+    def meeting_details(self, name):
+
+        query = f"What are {name}'s details?"
+
+        docs = self.db.similarity_search(query, 2)
+        docs = [d for d in docs if d.metadata['character_name'] == 'temo']
+        context = '\n'.join([d.page_content for d in docs])
+
+        human_message_prompt = HumanMessagePromptTemplate(
+            prompt=PromptTemplate(
+                template=self.qa_template ,
+                input_variables=["context", "query"],
+            )
+        )
+        chat_prompt_template = ChatPromptTemplate.from_messages([human_message_prompt])
+        chain = LLMChain(llm=self.chat_open_ai, prompt=chat_prompt_template)
+        response = chain.run({"context":context, "query":query})
+
+        return response
+
+
     def get_config(self):
         return self.config
 
@@ -73,17 +180,30 @@ class OpenaiLlm(LLM):
                 user_input.lower().startswith("multion")):
                 response = await self.multion_agent.action(user_input)
                 context += response
+        
+        self.agent.agent.llm_chain.prompt.messages[0].prompt.template = history[0].content # sys message
+        self.agent.agent.llm_chain.prompt.messages[2].prompt.template = user_input_template # user
+        self.agent.callbacks = [callback, audioCallback, StreamingStdOutCallbackHandler()]
 
-        # 2. Add user input to history
-        history.append(HumanMessage(content=user_input_template.format(
-            context=context, query=user_input)))
+        try:
+            response = self.agent.run(input=user_input)
+        except ValueError as e:
+            response = str(e)
+            if not response.startswith("Could not parse LLM output: "):
+                raise e
+            response = response.removeprefix("Could not parse LLM output: ")
 
-        # 3. Generate response
-        response = await self.chat_open_ai.agenerate(
-            [history], callbacks=[callback, audioCallback, StreamingStdOutCallbackHandler()],
-            metadata=metadata)
+        # # 2. Add user input to history
+        # history.append(HumanMessage(content=user_input_template.format(
+        #     context=context, query=user_input)))
+
+        # # 3. Generate response
+        # response = await self.chat_open_ai.agenerate(
+        #     [history], callbacks=[callback, audioCallback, StreamingStdOutCallbackHandler()],
+        #     metadata=metadata)
+
         logger.info(f'Response: {response}')
-        return response.generations[0][0].text
+        return response
 
     def _generate_context(self, query, character: Character) -> str:
         docs = self.db.similarity_search(query)

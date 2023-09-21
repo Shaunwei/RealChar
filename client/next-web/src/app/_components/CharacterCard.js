7bea0c1f05082c9e72f65070d95222b9ca0d54c5
Lei Q <amethystlei@gmail.com>
2023-09-21 01:35:49 -0700

    Add demo audio (#52)

---
+++ client/next-web/src/app/_components/CharacterCard.js
@@ -0,0 +1,93 @@
+`use client`
+
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Avatar,
+  Button
+} from '@nextui-org/react';
+import { FaPlay, FaStop } from 'react-icons/fa';
+import Image from 'next/image';
+import audioSvg from '@/assets/svgs/audio.svg';
+import { useRouter } from 'next/navigation';
+import lz from 'lz-string';
+
+import { useState, useRef } from 'react';
+
+export default function CharacterCard({
+  character
+}) {
+  const router = useRouter();
+  const [ isPlaying, setIsPlaying ] = useState(false);
+  const audioRef = useRef(null);
+
+  function handlePlay() {
+    let playPromise;
+    if (!isPlaying) {
+      //play
+      playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then(_ => {
+          setIsPlaying(true);
+        })
+        .catch(error => {
+          setIsPlaying(false);
+        })
+      }
+    } else {
+      audioRef.current.load();
+    }
+
+  }
+
+  return (
+    <Card className="p-2.5">
+      <CardBody className="p-0 text-center flex-row gap-2 md:flex-col">
+        <Avatar
+          radius="sm"
+          src={character.image_url}
+          className="w-20 h-20 md:w-44 md:h-44 md:mx-auto mt-2"
+        />
+        <div className="grow md:ml-0">
+          <p className="name text-base text-center h-12 flex flex-row justify-center items-center"><span>{character.name}</span></p>
+          <div className="flex justify-center mt-1 relative h-10">
+            <audio ref={audioRef} src={character.audio_url} preload="none">
+              Your browser does not support the audio tag.
+            </audio>
+            <Image
+              src={audioSvg}
+              alt=""
+              className="w-auto"
+            />
+            <Button
+              isIconOnly
+              variant="bordered"
+              radius="full"
+              color="white"
+              className="opacity-70 absolute"
+              onPress={handlePlay}
+            >
+            {!isPlaying ? (
+              <FaPlay/>
+            ) : (
+              <FaStop/>
+            )}
+            </Button>
+          </div>
+        </div>
+      </CardBody>
+      <CardFooter className="mt-5">
+        <Button
+          className="w-full font-light"
+          onPress={() => {
+            const compressedCharacter = lz.compressToEncodedURIComponent(
+              JSON.stringify(character)
+            );
+            router.push(`/conversation?character=${compressedCharacter}`);
+          }}
+        >Chat with me</Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- client/next-web/src/app/_components/ExploreTab.js
+++ client/next-web/src/app/_components/ExploreTab.js
@@ -1,58 +1,18 @@
-import { Card, CardBody, CardFooter } from '@nextui-org/card';
-import { Avatar } from '@nextui-org/avatar';
-import { Button } from '@nextui-org/button';
-import Image from 'next/image';
-import audioSvg from '@/assets/svgs/audio.svg';
-import { useRouter } from 'next/navigation';
-import lz from 'lz-string';
+import CharacterCard from './CharacterCard';
 
 export default function ExploreTab({ characters, isDisplay }) {
   const display = isDisplay ? 'flex' : 'hidden';
-  const router = useRouter();
 
   return (
     <section className={`flex flex-row flex-wrap justify-center mt-10 gap-5 ${display}`}>
       {
         characters?.map(character => (
-          <Card
+          <div
             key={character.character_id}
-            className="basis-72 md:basis-52 p-2.5">
-            <CardBody className="p-0 text-center flex-row md:flex-col">
-              <Avatar
-                radius="sm"
-                src={character.image_url}
-                className="w-20 h-20 md:w-44 md:h-44 md:mx-auto mt-2"
-              />
-              <div className="grow md:ml-0">
-                <p className="name text-base text-center mt-2 font-medium">{character.name}</p>
-                {/* <p className="intro text-xs text-center mt-2 font-light">
-                  &quot;I am burdened with glorious purpose.&quot;
-                </p> */}
-                <div className="flex justify-center mt-4">
-                  <audio>
-                    <source src={character.demo} type='audio/mp3' />
-                    Your browser does not support the audio tag.
-                  </audio>
-                  <Image
-                    priority
-                    src={audioSvg}
-                    alt=""
-                  />
-                </div>
-              </div>
-            </CardBody>
-            <CardFooter>
-              <Button
-                className="w-full font-light"
-                onPress={() => {
-                  const compressedCharacter = lz.compressToEncodedURIComponent(
-                    JSON.stringify(character)
-                  );
-                  router.push(`/conversation?character=${compressedCharacter}`);
-                }}
-              >Chat with me</Button>
-            </CardFooter>
-          </Card>
+            className="basis-72 md:basis-52"
+          >
+            <CharacterCard character={character}/>
+          </div>
         ))
       }
     </section>
